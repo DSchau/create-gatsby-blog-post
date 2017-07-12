e544065c93ebeca1c5076578d8e8e0f3245f46b9
Dustin Schau <dustinschau@gmail.com>
2017-07-12 11:47:37 -0500

    chore: minor tweaks to tests

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,30 +1,27 @@
-jest.mock('yargs');
-jest.mock('mz/fs');
-jest.mock('mz-modules/mkdirp');
+jest.unmock('mz/fs');
+
+import * as path from 'path';
 import * as yargs from 'yargs';
-import { spies } from 'mz/fs';
+import * as fs from 'mz/fs';
 
 const init = (argv = {}) => {
-  yargs.argv = Object.assign({ _: [] }, argv);
+  yargs.argv = argv;
   require('./cli');
 };
 
 describe('command line interface (CLI)', () => {
+  it('contains node bash header', async () => {
+    const contents = await fs.readFile(
+      path.resolve(__dirname, 'cli.ts'),
+      'utf-8'
+    );
+
+    expect(contents).toMatch('#!/usr/bin/env node');
+  });
+
   it('throws an error if folder name is not specified', () => {
-    expect(() => init()).toThrow(
+    expect(() => init({ _: [] })).toThrow(
       'A post title is required, e.g. `create-post "hello-world"`'
     );
-  });
-
-  it('does not write file if --dry', () => {
-    init({ _: ['hello world'], dry: true });
-
-    expect(spies.writeFile).not.toHaveBeenCalled();
-  });
-
-  it.skip('writes file', () => {
-    init({ _: ['hello world'] });
-
-    expect(spies.writeFile).toHaveBeenCalled();
   });
 });
