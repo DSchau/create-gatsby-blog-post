37a89acb294211557da4ff0cc416f98fb943895e
Dustin Schau <dustinschau@gmail.com>
2017-07-12 11:51:44 -0500

    test: use await when possible

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,69 +9,69 @@ describe('createPost', () => {
   });
 
   it('creates a directory', async () => {
-    createPost('hello-world', { date: new Date('10/08-1990') }).then(() => {
-      const call = mkdir.spy.mock.calls[0][0];
-      expect(call).toMatch('src/pages/10-08-1990-hello-world');
-    });
+    await createPost('hello-world', { date: new Date('10/08-1990') });
+
+    const call = mkdir.spy.mock.calls[0][0];
+    expect(call).toMatch('src/pages/10-08-1990-hello-world');
   });
 
   it('writes a blog post template', async () => {
-    createPost('hello-world', { date: new Date('10/08/1990') }).then(() => {
-      expect(fs.spies.writeFile).toHaveBeenCalledWith(
-        expect.any(String),
-        expect.any(String),
-        expect.any(String)
-      );
-    });
+    await createPost('hello-world', { date: new Date('10/08/1990') });
+
+    expect(fs.spies.writeFile).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.any(String),
+      expect.any(String)
+    );
   });
 
   it('returns a promise containing results', async () => {
-    createPost('hello-world', {
+    const results = await createPost('hello-world', {
       date: new Date('10/08/1990')
-    }).then(results => {
-      ['path', 'success'].forEach(key => expect(results[key]).toBeDefined());
     });
+
+    ['path', 'success'].forEach(key => expect(results[key]).toBeDefined());
   });
 
   describe('options', () => {
     it('does not write file if dry option', async () => {
-      createPost('hello-world', { dry: true }).then(({ dry }) => {
-        expect(dry).toBe(true);
-      });
+      const { dry } = await createPost('hello-world', { dry: true });
+
+      expect(dry).toBe(true);
     });
 
     it('can customize dateFormat', async () => {
-      createPost('hello-world', {
+      const { path } = await createPost('hello-world', {
         dry: true,
         dateFormat: 'YYYY'
-      }).then(({ path }) => {
-        expect(path).toMatch(/2017-hello-world/);
       });
+
+      expect(path).toMatch(/2017-hello-world/);
     });
 
     it('can customize date', async () => {
-      createPost('hello-world', {
+      const { path } = await createPost('hello-world', {
         dry: true,
         date: new Date('10/08/1990')
-      }).then(({ path }) => {
-        expect(path).toMatch(/10-08-1990/);
       });
+
+      expect(path).toMatch(/10-08-1990/);
     });
 
     it('can customize root', async () => {
       const root = '__heh_not_a_real_path__/pages';
-      createPost('hello-world', { dry: true, root }).then(({ path }) => {
-        expect(path).toMatch(root);
-      });
+      const { path } = await createPost('hello-world', { dry: true, root });
+
+      expect(path).toMatch(root);
     });
 
     it('does not dasherize if disabled', async () => {
-      createPost('hello world', {
+      const { path } = await createPost('hello world', {
         dry: true,
         dasherize: false
-      }).then(({ path }) => {
-        expect(path).toMatch('hello world');
       });
+
+      expect(path).toMatch('hello world');
     });
   });
 });
